1df3b8a6a4af86b52f890ce6dee0ece46e70f6e4
UselessName0 <gaetanoangrisani.04@gmail.com>
2025-07-02 19:35:03 +0200

    aggiunta di insert per iscrizioneSessione e iscrizioneCorso

--- AppUninaFoodLabProject/src/DAO/IscrizioneCorsoDAO.java
+++ AppUninaFoodLabProject/src/DAO/IscrizioneCorsoDAO.java
@@ -6,7 +6,33 @@ import java.util.List;
 
 import Database.DBManager;
 import Entities.IscrizioneCorso;
+import Entities.Corso;
+import Entities.Partecipante;
 
 public class IscrizioneCorsoDAO {
-
+	
+	public boolean insertIscrizioneCorso(IscrizioneCorso IscrizioneCorso_Input) {
+		String sql = "INSERT INTO uninafooddlab.iscrizionecorso(idcorso, idpartecipante, datadiiscrizione) VALUES (?, ?, ?)";
+			try(Connection conn = DBManager.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql)) {
+				
+				Corso tempCorso = IscrizioneCorso_Input.getC();
+				Partecipante tempPartecipante = IscrizioneCorso_Input.getP();
+				
+				pstmt.setString(1, tempCorso.getID_Corso());
+				pstmt.setString(2, tempPartecipante.getID_Partecipante());
+				pstmt.setDate(3, IscrizioneCorso_Input.getDataIscrizioneAsSQLDate());
+				
+				int rowsAffected = pstmt.executeUpdate();
+				return rowsAffected > 0;
+				
+			} catch(SQLException e) {
+				System.out.println("Errore durante l'inserimento dell'iscrizione al corso!");
+				return false;
+			}
+	}
+	
+	
+	
+	
 }
